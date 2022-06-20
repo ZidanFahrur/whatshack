0f76801c7cb67bb67662a13da26f28e9aa911f1e
ZidanFahrur <107808695+ZidanFahrur@users.noreply.github.com>
2022-06-21 03:00:23 +0700

    Create listener.sh

---
+++ listener.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+echo "
+<-- IndoSec -->
+####################
+# Whatsapp Exploit # By ./GeneratorPo
+#   Log Listener   # xxxxxxxxxxxx59@gmail.com
+####################
+Copyright (c) 2018 GeneratorPo";
+echo "";
+read -p "Server   : http://" server
+read -p "Log File : " file
+read -p "Delay (2-5) : " delay
+echo "";
+echo "[+] Collecting Information...";
+sleep 3;
+echo "[+] Server : http://$server";
+sleep 1;
+echo "[+] File   : $file";
+sleep 1;
+echo "[+] Delay  : $delay seconds";
+sleep 2;
+echo "[+] Executing http://$server/$file...";
+sleep 3;
+while [ true ]
+do
+curl http://$server/$file
+sleep $delay
+clear
